ac6f82c81f6b868180a0b250c99a8d14cc58a22c
marcusdjr <31329300+marcusdjr@users.noreply.github.com>
2022-10-19 16:08:46 -0400

    commit before creating model

--- demo/scripts/fit.py
+++ demo/scripts/fit.py
@@ -1,11 +1,18 @@
 # ---
 # jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.0
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
 
+# %%
 # Add description here
 #
 # *Note:* You can open this file as a notebook (JupyterLab: right-click on it in the side bar -> Open With -> Notebook)
@@ -21,11 +28,34 @@
 # %% tags=["parameters"]
 # If this task has dependencies, list them them here
 # (e.g. upstream = ['some_task']), otherwise leave as None.
-upstream = None
+upstream = ['get', 'sepal-feature', 'petal-feature']
 
 # This is a placeholder, leave it as None
 product = None
 
 
 # %%
-# your code here...
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn_evaluation import plot
+
+# %%
+raw = pd.read_csv(upstream['get']['data'])
+
+# %%
+raw
+
+# %%
+sepal = pd.read_csv(upstream['sepal-feature']['data'])
+
+# %%
+petal = pd.read_csv(upstream['petal-feature']['data'])
+
+# %%
+df = raw.join(sepal).join(petal)
+
+# %%
+df
+
+# %%
+X = df.drop('target', axis='co
--- demo/scripts/get.py
+++ demo/scripts/get.py
@@ -1,11 +1,18 @@
 # ---
 # jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.0
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
 
+# %%
 # Add description here
 #
 # *Note:* You can open this file as a notebook (JupyterLab: right-click on it in the side bar -> Open With -> Notebook)
@@ -13,8 +20,8 @@
 
 # %%
 # Uncomment the next two lines to enable auto reloading for imported modules
-# # %load_ext autoreload
-# # %autoreload 2
+# %load_ext autoreload
+# %autoreload 2
 # For more info, see:
 # https://docs.ploomber.io/en/latest/user-guide/faq_index.html#auto-reloading-code-in-jupyter
 
@@ -28,4 +35,12 @@ product = None
 
 
 # %%
-# your code here...
+from sklearn.datasets import load_iris 
+
+# %%
+df = load_iris(as_frame=True)['frame']
+
+# %%
+df.to_csv (product['data'], index=False)
+
+# %%
